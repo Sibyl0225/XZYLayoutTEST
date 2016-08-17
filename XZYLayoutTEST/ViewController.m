ecef8a56f5b3981809a5209dee0a050a167a8be8
MAC <mac@sion.local>
2016-08-17 10:41:41 +0800

    Initial Commit

---
+++ XZYLayoutTEST/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  XZYLayoutTEST
+//
+//  Created by MAC on 17/8/16.
+//  Copyright © 2016年 Mac0225. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
